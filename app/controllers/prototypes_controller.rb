3c83ed2f3e4a03e08067380d3c3e6a7fbb91a5c3
ryotanakatsuka <71533935+ryotanakatsuka@users.noreply.github.com>
2020-10-11 12:00:27 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,67 @@
+class PrototypesController < ApplicationController
+  before_action  :set_prototype,only:[:edit,:show]
+  before_action :authenticate_user!, only: [:edit]
+  before_action :move_to_edit, only: [:edit]
+
+  def index
+    @prototypes=Prototype.all
+  end
+
+
+  def new
+    @prototype=Prototype.new
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    if current_user.save
+       redirect_to root_path
+    else
+       render :new
+    end
+  end
+  
+  def show
+     @comment=Comment.new
+     @comments=@prototype.comments.includes(:user)
+  end
+  
+  def edit
+  end
+
+  def update
+       prototype=Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+        redirect_to prototype_path
+    else
+      render :edit
+    
+    end
+  end
+    def destroy
+      prototype=Prototype.find(params[:id])
+      prototype.destroy
+      redirect_to root_path
+    end
+  
+
+
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+    
+  end
+
+  def set_prototype
+    @prototype=Prototype.find(params[:id])
+  end
+
+  def move_to_edit
+    unless user_signed_in? && current_user.id == @prototype.user_id 
+
+      redirect_to action: :index
+    end
+  end
+end
+#&& current_user.id == @prototype.user_id 
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root to:"prototypes#index"
+  devise_for :users
+  resources :users,     only: :show
+  
+  resources  :prototypes do
+   resources  :comments, only: :create
+  end
+end
